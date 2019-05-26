fb13462c8e975181b487b4730d1f42b457d0f9cd
ccamposcr <ccamposcr@gmail.com>
2019-05-25 22:17:37 -0600

    services added

--- v2/hechoentiquicia/src/app/components/services/app.services.ts
+++ v2/hechoentiquicia/src/app/components/services/app.services.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.services.scss']
 })
 export class AppServicesComponent {
+  services = [
+      {
+        'item': 'Desarrollo de sitios web'
+      },
+      {
+        'item': 'Diseño de sitios web'
+      },
+      {
+        'item': 'Diseño gráfico'
+      },
+      {
+        'item': 'Desarrollo de aplicaciones móviles'
+      },
+      {
+        'item': 'Campañas de email masivas'
+      },
+      {
+        'item': 'Campañas en redes sociales'
+      }
+    ];
 }
